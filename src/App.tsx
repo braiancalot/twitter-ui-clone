77485097fa723201d23e7cf23265cffa89da2fa1
braiancalot <calot665@hotmail.com>
2023-03-12 14:04:08 -0300

    sidebar and timeline

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,79 @@
-import { Tweet } from "./components/Tweet"
+import twitterLogo from "./assets/logo-twitter.svg"
 import "./global.css"
+import { House, Hash, Bell, EnvelopeSimple, BookmarkSimple, FileText, User, DotsThreeCircle, Sparkle } from "@phosphor-icons/react";
+import { Tweet } from "./components/Tweet";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello new world</h1>
+      <div className="layout">
+        <aside className="sidebar">
+          <img src={twitterLogo} alt="Logo" className="logo" />
+
+          <nav className="main-navigation">
+            <a className="active" href="#">
+              <House weight="fill" />
+              Home
+            </a>
+            <a href="#">
+              <Hash />
+              Explore
+            </a>
+            <a href="#">
+              <Bell />
+              Notifications
+            </a>
+            <a href="#">
+              <EnvelopeSimple />
+              Messages
+            </a>
+            <a href="#">
+              <BookmarkSimple />
+              Bookmarks
+            </a>
+            <a href="#">
+              <FileText />
+              Lists
+            </a>
+            <a href="#">
+              <User />
+              Profile
+            </a>
+            <a href="#">
+              <DotsThreeCircle />
+              More
+            </a>
+          </nav>
+
+          <button className="new-tweet" type="button">Tweet</button>
+        </aside>
+
+        <div className="content">
+          <main className="timeline">
+            <div className="timeline-header">
+              Home
+              <Sparkle />
+            </div>
+
+            <form className="new-tweet-form">
+              <label htmlFor="tweet">
+                <img src="https://github.com/braiancalot.png" alt="Braian Calot" />
+
+                <textarea id="tweet" placeholder="What's happening?" />
+              </label>
+
+              <button type="submit">Tweet</button>
+            </form>
+
+            <div className="divisor" />
+
+            <Tweet />
+            <Tweet />
+            <Tweet />
+            <Tweet />
+          </main>
+        </div>
+      </div>
     </div>
   )
 }
--- src/components/Tweet.tsx
+++ src/components/Tweet.tsx
@@ -1,18 +1,41 @@
-interface TweetProps {
-  user: string
-  content: string
-  likes?: number
-}
+import { ChatCircle, ArrowsClockwise, Heart } from "@phosphor-icons/react";
+import "./Tweet.css"
 
-export function Tweet(props: TweetProps) {
-
-
+export function Tweet() {
   return (
-    <div className="tweet" style={{ borderRadius: 8 }}>
-      <h2>Tweet</h2>
-      <strong>{props.user}</strong>
-      <p>{props.content}</p>
-      <button>Like {props.likes ?? 0}</button>
-      <br />
-    </div>);
-}
+    <a href="#" className="tweet">
+      <img src="https://github.com/braiancalot.png" alt="Braian Calot" />
+
+      <div className="tweet-content">
+        <div className="tweet-content-header">
+          <strong>Braian Calot</strong>
+          <span>@braiancalot</span>
+        </div>
+
+        <p>Acabei de migrar um projeto React GIGANTE de create-react-app para Vite e os resultados foram:<br />
+          <br />
+          ✅ npm start: De 32s para 400ms (sim, demorava 30s)<br />
+          ✅ npm build: De 120s para 22s<br />
+          <br />
+          Além disso, troquei do Yarn para o PNPM e o install das deps mudou de 24s para 8s 🔥</p>
+
+        <div className="tweet-content-footer">
+          <button type="button">
+            <ChatCircle />
+            20
+          </button>
+
+          <button type="button">
+            <ArrowsClockwise />
+            20
+          </button>
+
+          <button type="button">
+            <Heart />
+            20
+          </button>
+        </div>
+      </div>
+    </a>
+  );
+} 
